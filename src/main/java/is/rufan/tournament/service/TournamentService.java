c5e3701d1fcc1dde5c12fcebf08f1b0a83b25a1c
Arnar Kari <arnarkari93@gmail.com>
2015-10-26 10:21:09 +0000

    added TournamentService interface and TournamentServiceException

---
+++ src/main/java/is/rufan/tournament/service/TournamentService.java
@@ -0,0 +1,17 @@
+package is.rufan.tournament.service;
+
+import is.rufan.tournament.domain.Tournament;
+
+import java.util.List;
+
+/**
+ * Created by arnarkari on 26/10/15.
+ *
+ * @author arnarkari
+ */
+public interface TournamentService {
+
+    public void addTournament(Tournament tournament) throws  TournamentServiceException;
+    public List<Tournament> getTournaments();
+    public Tournament getTournamentById(int tournamentid);
+}
