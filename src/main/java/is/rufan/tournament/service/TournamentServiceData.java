fd2704a69540ca7f37ab09524f55a413ce355685
Arnar Kari <arnarkari93@gmail.com>
2015-10-26 10:26:19 +0000

    Added empty TournamentServiceData

---
+++ src/main/java/is/rufan/tournament/service/TournamentServiceData.java
@@ -0,0 +1,25 @@
+package is.rufan.tournament.service;
+
+import is.rufan.tournament.domain.Tournament;
+
+import java.util.List;
+
+/**
+ * Created by arnarkari on 26/10/15.
+ *
+ * @author arnarkari
+ */
+public class TournamentServiceData implements TournamentService {
+
+    public void addTournament(Tournament tournament) throws TournamentServiceException {
+        
+    }
+
+    public List<Tournament> getTournaments() {
+        return null;
+    }
+
+    public Tournament getTournamentById(int tournamentid) {
+        return null;
+    }
+}
