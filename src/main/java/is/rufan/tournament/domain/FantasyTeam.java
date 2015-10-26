702f8a2ba17f14a8617c4f019f65e4c6b40dd20e
Eysteinn Gunnlaugsson <eysteinn@Eysteinns-MacBook-Pro.local>
2015-10-26 12:32:30 +0000

    domain fantasy team

---
+++ src/main/java/is/rufan/tournament/domain/FantasyTeam.java
@@ -0,0 +1,50 @@
+package is.rufan.tournament.domain;
+
+import java.util.List;
+
+/**
+ * Created by eysteinn on 26/10/15.
+ */
+public class FantasyTeam {
+    private int fantasyTeamId;
+    private int userId;
+    private List<Integer> players;
+
+    public FantasyTeam() {
+    }
+
+    public FantasyTeam(int fantasyTeamId, int userId, List<Integer> players) {
+        this.fantasyTeamId = fantasyTeamId;
+        this.userId = userId;
+        this.players = players;
+    }
+
+    public FantasyTeam(int userId, List<Integer> players) {
+        this.userId = userId;
+        this.players = players;
+    }
+
+    public int getFantasyTeamId() {
+        return fantasyTeamId;
+    }
+
+    public void setFantasyTeamId(int fantasyTeamId) {
+        this.fantasyTeamId = fantasyTeamId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public List<Integer> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Integer> players) {
+        this.players = players;
+    }
+}
