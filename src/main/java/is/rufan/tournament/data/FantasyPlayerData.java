81c147c5882c579051eb38629a68ebd70d1478f2
Eysteinn Gunnlaugsson <eysteinn@Eysteinns-MacBook-Pro.local>
2015-10-27 20:35:53 +0000

    flot

--- src/main/java/is/rufan/tournament/data/FantasyPlayerData.java
+++ src/main/java/is/rufan/tournament/data/FantasyPlayerData.java
@@ -6,8 +6,11 @@ import is.ruframework.data.RuData;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -79,12 +82,11 @@ public class FantasyPlayerData extends RuData implements FantasyPlayerDataGatewa
      * @param fantasy_teamid The id of the fantasy team
      * @return A list of fantasy players in a specific team
      */
-    public List<FantasyPlayer> getFantasyPlayerByTeamId(int fantasy_teamid) {
+    public List<Integer> getFantasyPlayersByTeamId(int fantasy_teamid) {
 
-        String sql = "Select * from fantasy_team_players where fantasy_teamid = ?";
+        String sql = "Select playerid from fantasy_team_players where fantasy_teamid = ?";
         JdbcTemplate query = new JdbcTemplate(getDataSource());
-        List<FantasyPlayer> fantasyPlayers = query.query(sql, new Object[]{ fantasy_teamid }
-                                                            , new FantasyPlayerRowMapper());
+        List<Integer> fantasyPlayers = query.queryForList(sql, new Object[]{ fantasy_teamid }, Integer.TYPE);
         return fantasyPlayers;
     }
 }
